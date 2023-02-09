ae52a2aa1fde06a6f2daf692c298cc274e2bf80a
Diguin20 <113214657+Diguin20@users.noreply.github.com>
2023-02-09 00:32:31 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -29,6 +29,31 @@ app.get('/financial_dashboard/:id/dashboard', function(req, res) {
     res.render('home')
 })
 
+//adicionado no dia 08/02
+
+const {eAdmin}= require('')
+
+app.post('/cadastro/updatecadastro', (req,res)=>{//adicionando usuarios a tabela
+    const nome= req.body.nome
+    const login = req.body.login
+    const senha= req.body.senha
+
+    const sql= `INSERT INTO usuarios(nome, login, senha) values(${nome},${login},${senha})`
+
+    conn.query(sql, (err)=>{
+        if(err){
+            console.log(err)
+        }
+        res.redirect('login')
+    })
+})
+
+
+
+
+
+//Fim do dia 08/02
+
 
 conn
   .sync()
